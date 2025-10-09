d7d64e972bb90a8a1548131e9ae02fd08bd32a0d
sshivahh <russelshivah16@gmail.com>
2025-10-09 15:05:55 +0700

    About section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,14 @@
 import HeroSection from "@/sections/home/HeroSection";
+import AboutSection from "@/sections/home/AboutSection";
 import ExpertiseSection from "@/sections/home/ExpertiseSection";
+
 
 export default function Home() {
   return (
     <div>
       <HeroSection />
+      <AboutSection />
       <ExpertiseSection />
-
     </div>
   );
 }
---
+++ src/sections/home/AboutSection.tsx
@@ -0,0 +1,27 @@
+import PlusSign from "@/components/PlusSign";
+
+export default function AboutSection() {
+  return (
+    <>
+      <section className="w-full h-[700px] border-b-2 border-black/25 flex">
+        <div className="flex-3 border-r-2 border-black/25 flex items-center justify-center">
+          <p className="font-telegraf font-medium text-4xl w-[80%]">
+            Empowering brands with systems that work — beautifully. From code to
+            cloud, we craft digital foundations that last. No fluff, only
+            function. Always one step ahead.
+          </p>
+        </div>
+        <div className="flex-2 flex items-center justify-center flex-col gap-20">
+          <p className="font-extralight text-2xl w-[70%]">
+            United by a passion for design, code, and interaction, our team
+            stands at the intersection of creativity and technology. We build
+            systems that don't just look good — they work beautifully.
+          </p>
+          <button className="px-10 py-5 text-4xl font-telegraf font-extralight relative border-2 border-black/25 hover:bg-black hover:text-white transition-all duration-300">
+            see our projects
+          </button>
+        </div>
+      </section>
+    </>
+  );
+}
