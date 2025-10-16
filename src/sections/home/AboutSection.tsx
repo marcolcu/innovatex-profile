0b243ba3903c5ef17ed15986bfdc72d4258a12dd
sshivahh <russelshivah16@gmail.com>
2025-10-16 12:13:03 +0700

    Responsive About and Expertise Section

--- src/sections/home/AboutSection.tsx
+++ src/sections/home/AboutSection.tsx
@@ -3,16 +3,16 @@ import PlusSign from "@/components/PlusSign";
 export default function AboutSection() {
   return (
     <>
-      <section className="w-full h-[700px] border-b-2 border-black/25 flex">
+      <section className="w-full h-[700px] border-b-2 border-black/25 flex flex-col xl:flex-row py-16 xl:py-0">
         <div className="flex-3 border-r-2 border-black/25 flex items-center justify-center">
-          <p className="font-telegraf font-medium text-4xl w-[80%]">
+          <p className="font-telegraf font-medium text-2xl xl:text-4xl w-[80%]">
             Empowering brands with systems that work — beautifully. From code to
             cloud, we craft digital foundations that last. No fluff, only
             function. Always one step ahead.
           </p>
         </div>
         <div className="flex-2 flex items-center justify-center flex-col gap-20">
-          <p className="font-extralight text-2xl w-[70%]">
+          <p className="font-extralight text-xl xl:text-2xl w-[70%]">
             United by a passion for design, code, and interaction, our team
             stands at the intersection of creativity and technology. We build
             systems that don't just look good — they work beautifully.
