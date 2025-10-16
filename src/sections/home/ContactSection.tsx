b57ad83d9e45eb53873d9c4a1610e6a5f022b3ff
sshivahh <russelshivah16@gmail.com>
2025-10-16 11:53:01 +0700

    Responsive Work and Contact Section

--- src/sections/home/ContactSection.tsx
+++ src/sections/home/ContactSection.tsx
@@ -1,18 +1,32 @@
 export default function ContactSection() {
   return (
     <>
-      <section className="w-full h-[200px] border-t-2 border-black/25 flex items-center justify-center">
-        <div className="flex-1 border-r-2 h-full border-black/25 flex justify-center items-center">
+      <section className="w-full h-[200px] border-t-2 border-black/25 xl:flex items-center justify-center hidden">
+        <div className="flex-2 border-r-2 h-full border-black/25 flex justify-center items-center">
           <a href="https://wa.me/6281586525868">
             <button className="px-10 py-5 text-4xl font-telegraf font-extralight relative border-2 border-black/25 hover:bg-black hover:text-white transition-all duration-300">
               contact us
             </button>
           </a>
         </div>
-        <div className="flex-1 flex h-full justify-center items-center">
-          <h1 className="font-telegraf text-[6rem] font-extralight  px-16">
+        <div className="flex-3 flex h-full justify-center items-center">
+          <h1 className="font-telegraf text-[4rem] font-extralight  px-16">
             +62 815-8652-5868
           </h1>
+        </div>
+      </section>
+      <section className="xl:hidden flex flex-col items-center justify-center border-t-2 border-black/25">
+        <div className="flex-1 flex h-full justify-center items-center">
+          <h1 className="font-telegraf text-[3rem] font-extralight  py-8">
+            +62 815-8652-5868
+          </h1>
+        </div>
+        <div className="flex-1 h-full  flex justify-center items-center py-8  w-full">
+          <a href="https://wa.me/6281586525868">
+            <button className="px-10 py-5 text-xl font-telegraf font-extralight relative border-2 border-black/25 hover:bg-black hover:text-white transition-all duration-300">
+              contact us
+            </button>
+          </a>
         </div>
       </section>
     </>
--- src/sections/home/WorkSection.tsx
+++ src/sections/home/WorkSection.tsx
@@ -3,13 +3,13 @@ import Image from "next/image";
 export default function WorkSection() {
   return (
     <>
-      <div className="h-[85vh] flex flex-col items-center justify-center">
+      <div className="h-fit flex flex-col items-center justify-center">
         <div className="flex-1 w-full border-b-2 border-black/25">
-          <h1 className="font-telegraf font-extralight text-[10rem] border-r-2 border-black/25 w-fit px-16">
+          <h1 className="font-telegraf font-extralight text-[6rem] xl:text-[10rem] border-r-2 border-black/25 w-fit px-16">
             Our Partners
           </h1>
         </div>
-        <div className="flex-4 w-fit flex">
+        <div className="flex-4 w-fit flex py-16">
           <div className="w-[100vw] h-full border-r-2 border-black/25 flex justify-center items-center flex-col">
             <Image
               src="/images/dharma-logo-red.webp"
@@ -17,7 +17,7 @@ export default function WorkSection() {
               width={400}
               height={500}
             ></Image>
-            <p className="text-5xl font-extralight mt-5">
+            <p className="text-5xl font-extralight mt-5 w-[80%] text-center">
               The Dharma Experience
             </p>
             <a href="https://thedharmaexperience.com">
